f41f980664fadbd1f57affed4af995d8371fa5b8
Sten Remmelg <sten.remmelg@gmail.com>
2020-09-18 13:11:57 +0200

    add base settings.paf, and required files for running theia-tools

---
+++ Theia Markerless Example/Templates/settings.php
@@ -0,0 +1,9 @@
+<?php
+/*==================================
+  Script settings
+  ==================================*/
+// TheiaTools
+$save_workspace = false; // Defines whether to save workspace or not when processing with Theia
+$theia_filter_type = "spline"; // Allowed filter types ["spline","moving_average"]
+$theia_filter_cutoff = 5; // Lowpass filter cut-off frequency in Hz
+?>
